d5d543f5cf201357b91c637a4eac50661f70c8bf
Francisco Matos <franciscomatos@MBPdeFrancisco.Home>
2022-04-02 17:10:01 +0100

    Organize navbar and filter container

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,17 +6,17 @@ import reddit from "../../Assets/reddit-logo.png"
 export const Navbar = () => {
   return (
     <div className="nav">
-        <div className="logo">
-            <img className="reddit-logo" src={reddit} />
-            <h1>Reddit <strong>Mini</strong></h1>
-        </div>
-        <div className="search">
-            <Search />
-        </div>
-        <div className="info">
-            <a className="about">About</a>
-            <a className="issue">Issue</a>
-        </div>
+      <div className="logo">
+        <img className="reddit-logo" src={reddit} />
+        <h1>Reddit <strong>Mini</strong></h1>
+      </div>
+      <div className="search">
+        <Search />
+      </div>
+      <div className="info">
+        <a className="about">About</a>
+        <a className="issue">Issue</a>
+      </div>
     </div>
   );
 };
